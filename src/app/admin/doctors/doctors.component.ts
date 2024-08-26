5b3a44fe6f4ab8e2d5d0d465460db240856cdc82
medaminemghirbi <mohamed.amine.mghirbi@gmail.com>
2024-08-26 17:51:23 +0100

    done

--- src/app/admin/doctors/doctors.component.ts
+++ src/app/admin/doctors/doctors.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AdminService } from 'src/app/services/admin.service';
@@ -10,11 +11,13 @@ import Swal from 'sweetalert2';
 })
 export class DoctorsComponent implements OnInit {
   doctors:any = []
+  filteredDoctors:any = []
+
   isLoading: boolean = false;
 
   messageErr: string = '';
   p:number = 1 ;
-  searchedKeyword!:string;
+  searchedKeyword: string = ''; // Initialized as an empty string
   constructor(private usersService: AdminService, private route: Router) { }
 
   ngOnInit(): void {
@@ -24,6 +27,7 @@ export class DoctorsComponent implements OnInit {
     this.isLoading = true;
     this.usersService.getDoctors().subscribe(data => {
       this.doctors = data;
+      this.filteredDoctors = data;
       this.isLoading = false;
     }, error => {
       console.error('Error fetching doctors', error);
@@ -31,6 +35,54 @@ export class DoctorsComponent implements OnInit {
     });
   }
 
+  searchDoctors(): void {
+    const trimmedKeyword = this.searchedKeyword ? this.searchedKeyword.trim() : '';
+  
+    if (trimmedKeyword) {
+      this.filteredDoctors = this.doctors.filter((doctor: { firstname: string, location:string, lastname:string  }) =>
+        doctor.firstname.toLowerCase().includes(trimmedKeyword.toLowerCase()) ||
+        doctor.location.toLowerCase().includes(trimmedKeyword.toLowerCase()) ||
+        doctor.lastname.toLowerCase().includes(trimmedKeyword.toLowerCase())
+
+      );
+    } else {
+      this.filteredDoctors = [...this.doctors];
+    }
+  
+    this.p = 1;
+  }
+  
+  opendataCSV() {
+
+    Swal.fire({
+      title: 'Careful this action will scrape doctors and import from CSV. It will take a lot of time. Are you sure?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      denyButtonText: `Don't save`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.isLoading = true;
+        this.usersService.reloadData().subscribe(
+          (data) => {
+            this.isLoading = false;
+            this.doctors = data.doctor; // Assuming data contains a `doctor` field
+            Swal.fire('Doctors successfully imported!', '', 'success').then(() => {
+              // Refresh the page after successful import
+              window.location.reload();
+            });
+          },
+          (err: HttpErrorResponse) => {
+            this.isLoading = false;
+            Swal.fire('Error', 'An error occurred while importing data.', err.error);
+          }
+        );
+      } else if (result.isDenied) {
+        Swal.fire('Changes are not saved', '', 'info');
+      }
+    });
+
+  }
   refreshDoctors(): void {
     this.loadDoctors();
   }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -18,7 +19,9 @@ export class AdminService {
   getDoctors(){
     return this.http.get(`${environment.urlBackend}`+'api/v1/doctors/')
   }
-
+  reloadData() {
+    return this.http.get<any>(`${environment.urlBackend}`+'api/v1/reload_data/')
+  }
   ArchiveDoctor(id:any){
     return this.http.delete(environment.urlBackend+'api/v1/doctors/' + id )
   }
